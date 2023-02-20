554d1b0a8cacf67529fce275ccd2c2c1c690f915
ksh564 <23445841+ksh564@users.noreply.github.com>
2023-02-21 00:42:17 +0900

    responsebody 어노테이션 공부

--- src/main/java/jpabook/jpashop/controller/HelloController.java
+++ src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +15,31 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // responseBody 는 json으로 기본반환 함
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        @Getter
+        @Setter
+        private String name;
     }
 }
